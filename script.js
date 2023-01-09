7183aa2668ade90b1f04ea0caa1c3252f2c3f95c
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-09 15:26:46 -0300

    finalização_da_calculadora_em_'js'_ultilizando_funcao_'eval'_que_faz_a_execucao_das_expressoes_do_display

--- script.js
+++ script.js
@@ -4,19 +4,60 @@ const teclasRes = document.querySelector(".res")
 const teclasDisplay = document.querySelector(".display")
 const ton= document.getElementById("ton")       // ultilizando 'getElementById' para pegar o id do 'html'
 const tlimpar = document.getElementById("tlimpar")
+const tigual = document.getElementById("tigual")
+
+
+let sinal = false
+let decimal = false
 
 teclasNum.forEach((el)=> {
     el.addEventListener("click", (evt)=> {      // adçionando o evento de 'click' para cada elemento
-        teclasDisplay.innerHTML += evt.target.innerHTML     /* quando teclas forem clicadas será adiçionada no display */
+        sinal = false             
+        if(evt.target.innerHTML == ",") {
+            if(!decimal) {
+                decimal = true
+                if(display.innerHTML == "0") {
+                    display.innerHTML = "0," 
+                } else {
+                    display.innerHTML += evt.target.innerHTML
+                }
+            }                            
+        } else {
+            if(display.innerHTML == "0") {
+                display.innerHTML = ""      // se 'display' for igual a zero será apagdo seu conteúdo
+            }
+
+            display.innerHTML += evt.target.innerHTML     /* quando teclas forem clicadas será adiçionada no display */
+        }
     })
 })
 
 teclasOp.forEach((el)=> {
     el.addEventListener("click", (evt)=> {
-        teclasDisplay.innerHTML += evt.target.innerHTML
+        if(!sinal) {
+            sinal = true    // toda vez que um operador for digitado a variável 'sinal' será true
+            if(display.innerHTML == "0") {
+                display.innerHTML = ""      // se 'display' for igual a zero será apagdo seu conteúdo
+            }
+            if(evt.target.innerHTML == "x") {
+                display.innerHTML += "*"        // substituio o 'X' por '*'
+            } else {
+                display.innerHTML += evt.target.innerHTML
+            }            
+        }
+
     })
 })
 
 tlimpar.addEventListener("click", (evt)=> {
+    sinal = false
+    decimal = false
     display.innerHTML = "0"       // limpa a tela quando clicar na tecla de id 'tlimpar'
+})
+
+tigual.addEventListener("click", (evt)=> {
+    sinal = false
+    decimal = false
+    const res = eval(display.innerHTML)     // vai pegar toda expressão do display e joga na função 'eval' que vai avaliar a expressão e executa-la
+    display.innerHTML = res     // jogando conteúdo de 'res' no display
 })
